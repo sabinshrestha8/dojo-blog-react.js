1145f1437dea91c58448d67f4004a085fe29d4ea
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-02 19:12:58 +0545

    #3-Dynamic values in templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // step 1: storing string in a variable
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // const person = { name: 'yoshi', age: 30 };  // cannot output objects & booleans in browser directly
+  const link = "https://www.google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        {/* step 2: outputting the stored value dynamically */}
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+
+        {/* <p>{ person }</p> */}
+        
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+         <a href={ link }>Google Site</a>
+      </div>
     </div>
   );
 }
