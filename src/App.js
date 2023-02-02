78d842786df6267d13e710a8d83736c668061ab3
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-02 21:39:12 +0545

    #4-Multiple Components created & nest them in root component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
+// step 1: importing Navbar & Home component 
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  // step 1: storing string in a variable
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };  // cannot output objects & booleans in browser directly
-  const link = "https://www.google.com";
-
   return (
     <div className="App">
+      {/* step 2: injecting Navbar component inside root component */}
+      <Navbar />
       <div className="content">
-        {/* step 2: outputting the stored value dynamically */}
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-        
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-         <a href={ link }>Google Site</a>
+        {/* step 2: nesting Home component inside root component */}
+        <Home />
       </div>
     </div>
   );
