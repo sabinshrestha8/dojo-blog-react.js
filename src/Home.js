c559557573037e4ac1c756a4bc33f752108165ff
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-03 15:49:13 +0545

    #7-Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={ handleClick }>Click me</button>
+            <button onClick={ (e) => {
+                // console.log('hello')
+                handleClickAgain('mario', e)
+            } }>Click me again</button>
         </div>
      );
 }
