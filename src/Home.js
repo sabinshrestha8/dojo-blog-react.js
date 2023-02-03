7f705477f2853478da9cafa0924d24d7bb2df10b
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-03 22:38:24 +0545

    #8-using state (useState hook)

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,29 @@
-const Home = () => {
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
-    }
+import { useState } from "react";
 
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e.target);
+const Home = () => {
+    // non-reactive value
+    // let name = 'mario';
+
+    // const handleClick = () => {
+    //     name = 'luigi';
+    //     console.log(name);
+    // }
+
+    // create a reactive value
+    // The useState hook returns two value to us which is destructured an array
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge(26);
     }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={ handleClick }>Click me</button>
-            <button onClick={ (e) => {
-                // console.log('hello')
-                handleClickAgain('mario', e)
-            } }>Click me again</button>
         </div>
      );
 }
