0e797f82f21aac730627bb832f017b1ad4b2a192
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-04 12:16:33 +0545

    #10-Outputting lists in react

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-    // non-reactive value
-    // let name = 'mario';
-
-    // const handleClick = () => {
-    //     name = 'luigi';
-    //     console.log(name);
-    // }
-
-    // create a reactive value
-    // The useState hook returns two value to us which is destructured an array
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('luigi');
-        setAge(26);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={ handleClick }>Click me</button>
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            )) }
         </div>
      );
 }
