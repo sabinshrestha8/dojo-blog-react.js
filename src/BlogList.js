801440f1f0848ac0dadc89bcbd2adaf9bdc3e328
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-06 20:29:37 +0545

    #17-Fetching data with useEffect & using conditional templating

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {    // Destructuring the props
+const BlogList = ({ blogs, title }) => {    // Destructuring the props
     return (
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -6,7 +6,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {    // Destructuring the p
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
-                    <button onClick={ () => handleDelete(blog.id) }>delete blog</button>
                 </div>
             )) }
         </div>
--- src/Home.js
+++ src/Home.js
@@ -2,33 +2,23 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    /* have to be careful while changing state inside use effect hook 
-        i.e. could end up in continuous loop */
-
-    // use effect hook fires on every render
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]);     // setting name as dependency and useEffect hook will trigger as the state of the name changes
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            // console.log(data);
+            setBlogs(data);
+        })
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={ blogs } title="All Blogs" handleDelete={ handleDelete } />
-            <button onClick={() => setName('luigi')}>change name</button>
-            <p>{ name }</p>
+            {/* using conditional templating in react */}
+            { blogs && <BlogList blogs={ blogs } title="All Blogs" /> }
         </div>
      );
 }
