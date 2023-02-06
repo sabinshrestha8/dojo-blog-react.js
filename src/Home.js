344dd25f730168a1a5f037bd3da279fe94730b57
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-06 20:50:55 +0545

    #18-Conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,26 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            // console.log(data);
-            setBlogs(data);
-        })
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    // console.log(data);
+                    setBlogs(data);
+                    setIsPending(false);
+                });
+        }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            {/* when isPending is true it shows Loading... message  */}
+            { isPending && <div>Loading...</div> }
             {/* using conditional templating in react */}
             { blogs && <BlogList blogs={ blogs } title="All Blogs" /> }
         </div>
