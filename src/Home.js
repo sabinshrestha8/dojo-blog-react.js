c60c7d037bd35d42e5fd39a31e9fa7e2e1e8ef08
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-08 16:38:49 +0545

    #19-Handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,35 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if(!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
                     return res.json();
                 })
                 .then(data => {
                     // console.log(data);
                     setBlogs(data);
                     setIsPending(false);
-                });
+                    setError(null);
+                })
+                .catch((err) => {
+                    // console.log(err.message);
+                    setIsPending(false);
+                    setError(err.message);
+                })
         }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
             {/* when isPending is true it shows Loading... message  */}
             { isPending && <div>Loading...</div> }
             {/* using conditional templating in react */}
