b3269b26a72ecf4ae9fc5ba729a62e05a7b0e170
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-09 22:29:58 +0545

    #23-Router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,14 @@
+/* import the Link tag i.e. Link component from react-router-dom package */
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <Link to="/">Home</Link>
                 {/* adding inline styles */}
-                <a href="/create">New Blog</a>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
