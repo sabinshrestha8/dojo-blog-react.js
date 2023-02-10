8cbfc47f9f13798b3876873d585a602b886ec0c9
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-10 19:47:19 +0545

    #24-Use effect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,10 +6,17 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+
+        /* we have an abort controller object and what we can do with it
+        is we can associate it with a specific fetch request and once
+        we 've associated it with a fetch we can use that abort controller
+        to stop the fetch */
+        const abortController = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortController.signal })  // associating abortController with the fetch
                 .then(res => {
-                    if(!res.ok) { // error coming back from server
+                    if(!res.ok) {
                         throw Error('could not fetch the data for that resource');
                     }
                     return res.json();
@@ -19,14 +26,24 @@ const useFetch = (url) => {
                     setIsPending(false);
                     setError(null);
                 })
-                .catch((err) => {
-                    // auto catches network / connection error
-                    setIsPending(false);
-                    setError(err.message);
-                    setData(null);
+                .catch(err => {
+                    if(err.name === 'AbortError') {
+                        console.log('fetch aborted');
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                        setData(null);
+                    }
                 })
         }, 1000);
-    }, []);
+
+        /* when the component that uses this useEffect() or usefetch()
+        hook unmounts it fires that returned cleanup function */
+
+        // return () => console.log('cleanup');
+        return () => abortController.abort();
+
+    }, [url]);
 
     return { data, isPending, error };
 }
