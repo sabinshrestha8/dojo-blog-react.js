ad25bc899af49e9bf76b850e256ffd36ad5cb510
sabinshrestha8 <sabinshrestha8888@gmail.com>
2023-02-11 16:21:46 +0545

    #32-Introducing 404 pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -20,6 +21,15 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            {/* For 404 page the route path should be "*" which means
+            catch any other routes and it goes in the bottom of the
+            template bcoz otherwise it's going to match any route that
+            comes in if it goes at the top, if it goes at the bottom if
+            none of these above route match then this will match it 
+            regardless & its kind of like catch-all route */}
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
